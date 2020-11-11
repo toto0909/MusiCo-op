8575994e8a095b3f46384aa22ce16a42a0d25d9e
toto <toto0909@icloud.com>
2020-11-11 15:54:44 +0900

    モジュール化テスト

--- include/php/db_access.php
+++ include/php/db_access.php
@@ -1,20 +1,72 @@
 <?php
-//DBに接続する
-//DBにアクセスし、結果をテーブルで出力する
-require_once('db.php');
-try {
-   $pdo = new PDO($dsn, $user, $password);
-   $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   echo "データベース{$dbName}に接続しました。";
-   $pdo = NULL;
-} catch (Exception $e) {
-   echo "<span class='error'>エラーがありました。</span><br>";
-   echo $e->getMessage();
-   exit();
-}
+{*   *}
 
-header('Content-Type: text/html; charset=utf-8');
+require_once('db.php');
+$db = new DB();
+$personal = $db->get_personal();
+$organization = $db->get_organization();
+$instrumental = $db->get_instrumental();
+$address = $db->get_address();
+$SNS = $db->get_SNS();
+
+echo 'personal' .PHP_EOL;
+echo "<pre>";
+var_dump($personal) .PHP_EOL;
+echo "</pre>";
+
+echo 'organization' .PHP_EOL;
+echo "<pre>";
+var_dump($organization) .PHP_EOL;
+echo "</pre>";
+
+echo 'instrumental' .PHP_EOL;
+echo "<pre>";
+var_dump($instrumental) .PHP_EOL;
+echo "</pre>";
+
+echo 'address' .PHP_EOL;
+echo "<pre>";
+var_dump($address) .PHP_EOL;
+echo "</pre>";
+
+echo 'SNS' .PHP_EOL;
+echo "<pre>";
+var_dump($SNS) .PHP_EOL;
+echo "</pre>";
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <title>musico-opデータベーステスト</title>
+        <link rel="stylesheet" type="text/css" href="/css/styles.css" >
+
+
+    </head>
+
+    <body>
+        <table>
+        <tr>
+            <td>ID　</td>
+            <td><?php echo($personal['musicoop_ID']);?></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>名前　</td>
+            <td><?php echo($personal['name']);?></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>生年月日　</td>
+            <td><?php echo($personal['birthday']);?></td>
+            <td></td>
+        </tr>
+        </table>
+    </body>
+</html>

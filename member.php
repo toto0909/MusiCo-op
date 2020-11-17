1521fc7f6a56cf400e0b1983216c73549aefb058
toto <toto0909@icloud.com>
2020-11-18 00:22:56 +0900

    member2

--- member.php
+++ member.php
@@ -20,7 +20,17 @@ $db = new DB();
     <title>メンバー・団体情報 Member</title>
     <!--set tytle for even pages-->
     <style>
+        .card {
+            color: #000;
+        }
 
+        .menu {
+            position: sticky;
+            top: 0;
+        }
+        .img{
+            object-fit: cover;
+        }
     </style>
 </head>
 
@@ -32,53 +42,102 @@ $db = new DB();
         $personal = $db->get_personal();
         $organization = $db->get_organization();
         $instrumental = $db->get_instrumental();
-
-        echo 'personal' . PHP_EOL;
-        echo "<pre>";
-        var_dump($personal) . PHP_EOL;
-        echo "</pre>";
-
-        $members = count($personal); //合計人数の取得
-        $rows = floor($members / 6); //行数
-        $cols = $members % 6;        //最終行に出力される人数
-        echo $members;
-        echo $rows;
-        echo $cols;
+        $address_ = $db->get_address();
         ?>
-        <!--1行$even_rows人ずつだす(<$even_rowsの場合$colsの数だけ出す) -->
+        <!--1行$even_rows人ずつだす(<$even_rowsの場合$colsの数だけ出す 出す順番はランダム) -->
         <!-- PC版 -->
-        <?php foreach ($personal as $value) : ?>
-            <div class="container text-center">
-                <div class="row">
-                    <div class="col-sm-2">
-                        <?php $org = $value['organization_ID'] ?>
-                        <?php $adr = $value['address_ID'] ?>
-                        <?php $ins = $value['instrumental_ID'] ?>
-                        <img src="/src/user/<?php echo $value['img_URL']; ?>">
-                        <!--画像-->
-                        <span><?php echo $value['name']; ?></span>
-                        <!--名前-->
-                        <span><?php echo $organization[$org]; ?></span>
-                        <!--組織-->
-                        <span><?php echo $functions->get_grade($value['grade']); ?></span>
-                        <!--学年-->
-                        <span><?php
-                                if ($value['sex'] = 0) {
-                                    echo '男性';
-                                } else {
-                                    echo '女性';
-                                }
-                                ?></span>
-                        <!--性別-->
-                        <span><?php echo $address_[$adr]; ?></span>
-                        <!--住所-->
-                        <span><?php echo $instrumental[$ins]; ?></span>
-                        <!--楽器-->
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-2">
+                    <div class="menu">
+                        <p>メニューバー</p>
+                        <p>検索窓</p>
+                        <p>大学検索</p>
+                        <p>学年検索</p>
+                        <p>性別検索</p>
+                        <p>楽器検索</p>
+                        <p>住所検索</p>
+                        <p>検索ボタン</p>
                     </div>
-
+                </div>
+                <div class="col-sm-10 overflow-auto">
+                    <div class="row">
+                        <?php foreach ($personal as $value) : ?>
+                            <?php $org = $value['organization_ID'] ?>
+                            <?php $adr = $value['address_ID'] ?>
+                            <?php $ins = $value['instrumental_ID'] ?>
+                            <?php $grd = $functions->get_grade($value['grade']); ?>
+                            <div class="col-sm-3 col-md-2 pt-3 pl-0 pr-2">
+                                <a href="#">
+                                    <div class="card text-center">
+                                        <img class="card-img-top img-fluid" src="/src/user/<?php echo $value['img_URL']; ?>" alt="person">
+                                        <div class="card-body">
+                                            <h5 class="card-title pb-1"><?php echo $value['name']; ?></h5>
+                                            <h6 class="card-subtitle text-muted pb-1"><?php echo $organization[$org - 1]['org_name']; ?></h6>
+                                            <h6 class="card-subtitle text-muted pb-1"><?php echo $grd ?></h6>
+                                            <h6 class="card-subtitle text-muted pb-1"><?php echo $instrumental[$ins - 1]['ins_name']; ?></h6>
+                                            <h6 class="card-subtitle text-muted pb-1"><?php echo $address_[$adr - 1]['adr_name']; ?></h6>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
                 </div>
             </div>
-        <?php endforeach; ?>
+        </div>
+
+        <!-- SP版 -->
+        <div class="container mb-2 mt-2">
+            <div class="members">
+                <?php foreach ($personal as $value) : ?>
+                    <?php $org = $value['organization_ID'] ?>
+                    <?php $adr = $value['address_ID'] ?>
+                    <?php $ins = $value['instrumental_ID'] ?>
+                    <?php $grd = $functions->get_grade($value['grade']); ?>
+                    <div class="row">
+                        <div class="col-xs-4 ml-2">
+                            <a href="#"><img src="/src/user/<?php echo $value['img_URL']; ?>" alt="person" width="100" height="100" class="img-fluid"></a>
+                        </div>
+                        <div class="col-xs-8 ml-2 mx-auto">
+                            <div class="container text-center"><?php echo $value['name']; ?></div>
+                            <div class="table-responsive overflow-auto text-center">
+                                <table class="table table-condensed member_table mb-0 pb-0 height=100">
+                                    <tbody>
+                                        <tr>
+                                            <td class="pt-0 pb-0"><?php echo $value['name']; ?></td>
+                                        </tr>
+                                        <tr>
+                                            <td class="pt-0 pb-0"><?php echo $organization[$org - 1]['org_name']; ?></td>
+                                        </tr>
+                                        <tr>
+                                            <td class="pt-0 pb-0"><?php echo $grd ?></td>
+                                        </tr>
+                                        <tr>
+                                            <td class="pt-0 pb-0"><?php echo $address_[$adr - 1]['adr_name']; ?></td>
+                                        </tr>
+                                        <tr>
+                                            <td class="pt-0 pb-0"><?php
+                                                                    if ($value['sex'] == 0) {
+                                                                        echo '男性';
+                                                                    } else {
+                                                                        echo '女性';
+                                                                    }
+                                                                    ?></td>
+                                        </tr>
+                                        <tr>
+                                            <td class="pt-0 pb-0"><?php echo $instrumental[$ins - 1]['ins_name']; ?></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+
+
     </main>
     <!--Mojuled footer-->
     <?php include(dirname(__FILE__) . '/include/footer.php'); ?>

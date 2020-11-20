85e477b6956689758de83ccbb18d34c8edfd8020
toto <toto0909@icloud.com>
2020-11-20 22:42:03 +0900

    profile2

--- include/functions.php
+++ include/functions.php
@@ -32,6 +32,23 @@ class Functions{
         }
         return $r_grade;
     }
+
+    //英語文字列の" "を"_"に変換 -> 引用画像フォルダ指定に使用
+    function replace_name(string $e_name){
+        return str_replace(" ","_",$e_name);
+    }
+
+    //"_"で置き換えられた名前に誕生日情報(ex Yuki_Takeda + 1993-09-09)を連結 => 個人プロフィール画像フォルダ名取得
+    function get_images(string $name , string $birthday){
+        $replaced_name = $this->replace_name($name);
+        return $replaced_name.$birthday;
+    }
+    //"xxxx-xx-xx"の文字列を"xxxx年xx月xx日"に変換
+    function get_birthday(string $birthday){
+        $birthday = substr_replace($birthday, "年", 1,5);
+        $birthday = substr_replace($birthday, "月", 1,8);
+        return $birthday."日";
+    }
 }
 
 ?>
